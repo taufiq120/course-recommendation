8d2946a961df29c95c439d113fcbd740d4d9264e
Taufiq Ahmed I <125111367+taufiq120@users.noreply.github.com>
2024-10-10 17:28:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.neighbors import NearestNeighbors
 
-# Load the dataset
+# Loading the dataset
 def load_data():
     data = pd.read_csv('Coursera.csv')
     data['Course Description'] = data['Course Description'].fillna('')
@@ -12,15 +12,15 @@ def load_data():
 
 data = load_data()
 
-# Streamlit UI
+# Creating the Streamlit UI
 
 st.title("Course Recommendation System")
 st.write("Select a course to find similar ones.")
-# Dropdown to select a course
+#Making Dropdown to select a course
 course_titles = data['Course Name'].values
 selected_course = st.selectbox("Choose a Course", course_titles)
 
-# Build the KNN model
+# Building the KNN model
 @st.cache_data
 def build_knn_model(data):
     tfidf = TfidfVectorizer(stop_words='english') #gives weight to topics
@@ -33,14 +33,14 @@ def build_knn_model(data):
 
 knn, tfidf_matrix = build_knn_model(data)
 
-# Get KNN recommendations
+# Using the knn model to Get recommendations
 def get_knn_recommendations(title, knn_model, tfidf_matrix, data):
     idx = data[data['Course Name'] == title].index[0]
     distances, indices = knn_model.kneighbors(tfidf_matrix[idx], n_neighbors=6)
     similar_courses = indices.flatten()[1:]
     return data['Course Name'].iloc[similar_courses]
 
-# Display recommendations
+# Displaying the recommendations
 if selected_course:
     recommendations = get_knn_recommendations(selected_course, knn, tfidf_matrix, data)
     st.write(f"Top 5 recommended courses similar to '{selected_course}':")
